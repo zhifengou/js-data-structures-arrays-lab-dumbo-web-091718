621d784a5e364dc48eecc913e9cf379d7e337c83
zhifeng ou <zhifengou@hotmail.com>
2018-10-28 18:37:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,8 @@ function prependDriver(name){
   return[name,...drivers];
 }
 function removeLastDriver(){
-  // return drivers.slice(0,drivers.length-1);
-  return drivers.splice(0,(drivers.length-1));
+  return drivers.slice(0,drivers.length-1);
 }
 function removeFirstDriver(){
-  // return drivers.slice(1);
-  return drivers.splice(1,drivers.length-1);
+  return drivers.slice(1);
 }
